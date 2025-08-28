8a7a415e62dc84c13ac932f413fcd1db7ce7b3ad
dotman227 <donsteve479.os@gmail.com>
2025-08-28 14:07:30 +0100

    version 1

---
+++ app.js
@@ -0,0 +1,21 @@
+const myButton = document.getElementById("blackBtn");
+
+myButton.addEventListener("click", function(){
+  document.body.style.backgroundColor = "black";
+});
+
+const mySwitch = document.getElementById("blueBtn");
+
+mySwitch.addEventListener("click", function(){
+  document.body.style.color = "white";
+});
+
+const students = ["toluwani", "abimbola", "micheal", "emmanuel", "frederick"];
+
+const studentList = document.getElementById("studentList");
+
+students.forEach(function(student){
+  const li = document.createElement("li");
+  li.textContent = student;
+  studentList.appendChild(li);
+});
